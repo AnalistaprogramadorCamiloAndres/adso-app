9a36d8fd3a3ac09117d9afbba1513d66cbe7d3e7
CamiloRodriguez <camiloa_rodrigueze@soy.sena.edu.co>
2024-09-04 22:43:03 -0500

    Se crearon los metodos y rutas del proyecto

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ClienteController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route to register a new user 
+ // Assuming ClienteController is in the App\Http\Controllers namespace
+Route::get('/api/clientes/', [ClienteController::class, 'getData']);
+Route::put('/api/clientes/', [ClienteController::class, 'update']);
+Route::post('/api/clientes/', [ClienteController::class, 'save']);
+Route::delete('/api/clientes/', [ClienteController::class, 'delete']);
