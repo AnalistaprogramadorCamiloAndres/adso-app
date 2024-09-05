6b1efe07d749f95e9b85d28aaa782f2fe6249660
CamiloRodriguez <camiloa_rodrigueze@soy.sena.edu.co>
2024-09-04 23:26:02 -0500

    Se crean todos los modulos y rutas del proyecto

---
+++ app/Http/Controllers/Empleado.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class Empleado extends Controller
+{
+    public function getData (Request $request){
+        return response()->json([
+               'status' => '200',
+               'message' => 'data..'
+           ]);
+              }
+       /**    /**
+     ** Store a newly created resource in storage.
+     */
+public function save(Request $request)
+{
+    return response()->json([
+        'status' => '200',
+        'message' => 'Guardado con exito',
+    ]);
+}
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        return response() -> json([
+            'status' => '200',
+           'message' => 'Actualizado con exito',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function delete(request $request)
+    {
+        return response() -> json([
+            'status' => '200',
+           'message' => 'Eliminado con exito',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\Empleado;
+use App\Http\Controllers\EnvioDeProductos;
+use App\Http\Controllers\Facturacion;
+use App\Http\Controllers\IngresoAlSistema;
+use App\Http\Controllers\InspeccionDeCalidad;
+use App\Http\Controllers\LoteDeProduccion;
+use App\Http\Controllers\Producto;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,3 +33,38 @@ Route::get('/api/clientes/', [ClienteController::class, 'getData']);
 Route::put('/api/clientes/', [ClienteController::class, 'update']);
 Route::post('/api/clientes/', [ClienteController::class, 'save']);
 Route::delete('/api/clientes/', [ClienteController::class, 'delete']);
+
+Route::get('/api/empleado/', [Empleado::class, 'getData']);
+Route::put('/api/empleado/', [Empleado::class, 'update']);
+Route::post('/api/empleado/', [Empleado::class, 'save']);
+Route::delete('/api/empleado/', [Empleado::class, 'delete']);
+
+Route::get('/api/enviodeproductos/', [EnvioDeProductos::class, 'getData']);
+Route::put('/api/enviodeproductos/', [EnvioDeProductos::class, 'update']);
+Route::post('/api/enviodeproductos/', [EnvioDeProductos::class, 'save']);
+Route::delete('/api/enviodeproductos/', [EnvioDeProductos::class, 'delete']);
+
+Route::get('/api/facturacion/', [Facturacion::class, 'getData']);
+Route::put('/api/facturacion/', [Facturacion::class, 'update']);
+Route::post('/api/facturacion/', [Facturacion::class, 'save']);
+Route::delete('/api/facturacion/', [Facturacion::class, 'delete']);
+
+Route::get('/api/ingresoalsistema/', [IngresoAlSistema::class, 'getData']);
+Route::put('/api/ingresoalsistema/', [IngresoAlSistema::class, 'update']);
+Route::post('/api/ingresoalsistema/', [IngresoAlSistema::class, 'save']);
+Route::delete('/api/ingresoalsistema/', [IngresoAlSistema::class, 'delete']);
+
+Route::get('/api/inspecciondecalidad/', [InspeccionDeCalidad::class, 'getData']);
+Route::put('/api/inspecciondecalidad/', [InspeccionDeCalidad::class, 'update']);
+Route::post('/api/inspecciondecalidad/', [InspeccionDeCalidad::class, 'save']);
+Route::delete('/api/inspecciondecalidad/', [InspeccionDeCalidad::class, 'delete']);
+
+Route::get('/api/lotedeproduccion/', [LoteDeProduccion::class, 'getData']);
+Route::put('/api/lotedeproduccion/', [LoteDeProduccion::class, 'update']);
+Route::post('/api/lotedeproduccion/', [LoteDeProduccion::class, 'save']);
+Route::delete('/api/lotedeproduccion/', [LoteDeProduccion::class, 'delete']);
+
+Route::get('/api/producto/', [Producto::class, 'getData']);
+Route::put('/api/producto/', [Producto::class, 'update']);
+Route::post('/api/producto/', [Producto::class, 'save']);
+Route::delete('/api/producto/', [Producto::class, 'delete']);
